09d310c69b8aa5c89ace783b40ae5aab35bb7f02
Auri Munoz <amunozhe@redhat.com>
2024-02-14 11:10:06 +0100

    Call api when loading page + use thymeleaf collections

--- src/main/java/org/rock/spring/DispatcherController.java
+++ src/main/java/org/rock/spring/DispatcherController.java
@@ -16,6 +16,7 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -35,23 +36,27 @@ public class DispatcherController {
     @GetMapping
     public String guitarHero(Model model) throws URISyntaxException, MalformedURLException {
         model.addAttribute("appName", appName);
+        List<String> rockStarImages = new ArrayList<>();
 
-        ServiceInstance serviceInstance = stork.getService("guitar-hero-service").selectInstance().await().atMost(Duration.ofSeconds(5));
+        for(int i=1; i<=10; i++) {
+            ServiceInstance serviceInstance = stork.getService("guitar-hero-service").selectInstance().await().atMost(Duration.ofSeconds(5));
 
-        String url = UriComponentsBuilder.newInstance()
-                .scheme("http")
-                .host(serviceInstance.getHost())
-                .port(serviceInstance.getPort())
-                .path(serviceInstance.getPath().orElse(""))
-                .build()
-                .toUri()
-                .toURL().toString();
+            String url = UriComponentsBuilder.newInstance()
+                    .scheme("http")
+                    .host(serviceInstance.getHost())
+                    .port(serviceInstance.getPort())
+                    .path(serviceInstance.getPath().orElse(""))
+                    .build()
+                    .toUri()
+                    .toURL().toString();
 
-        System.out.println(" ---------- Selected Rock Start: " + url);
+            System.out.println(" ---------- Selected Rock Start: " + url);
 
-        String rockStar = restTemplate.getForObject(
-                url, String.class);
-        model.addAttribute("imageBytes", rockStar);
+            String rockStar = restTemplate.getForObject(
+                    url, String.class);
+            rockStarImages.add(rockStar);
+        }
+        model.addAttribute("imageBytes", rockStarImages);
         return "index";
     }
 
