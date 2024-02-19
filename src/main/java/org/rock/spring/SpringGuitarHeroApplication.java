2c68fd1dcde14cfd840af9de91af1dda60c50c88
Auri Munoz <amunozhe@redhat.com>
2024-02-19 18:26:39 +0100

    Use stork SB support and application.properties file for Stork config

--- src/main/java/org/rock/spring/SpringGuitarHeroApplication.java
+++ src/main/java/org/rock/spring/SpringGuitarHeroApplication.java
@@ -2,14 +2,22 @@ package org.rock.spring;
 
 import io.smallrye.stork.Stork;
 import io.smallrye.stork.api.ServiceDefinition;
-import io.smallrye.stork.servicediscovery.consul.ConsulConfiguration;
+import io.smallrye.stork.api.config.ServiceConfig;
+import io.smallrye.stork.springboot.SpringBootConfigProvider;
+import io.smallrye.stork.spi.config.ConfigProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.Environment;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringGuitarHeroApplication {
@@ -29,7 +37,8 @@ public class SpringGuitarHeroApplication {
     public Stork stork() {
         Stork.initialize();
         Stork stork = Stork.getInstance();
-        stork.defineIfAbsent("guitar-hero-service", ServiceDefinition.of(new ConsulConfiguration()));
+        // stork.defineIfAbsent("guitar-hero-service", ServiceDefinition.of(new ConsulConfiguration()));
         return stork;
     }
+
 }
