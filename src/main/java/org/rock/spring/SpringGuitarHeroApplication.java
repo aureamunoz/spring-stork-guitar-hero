36b0c30e425ca052a9051509d933f3e5f8268a47
Auri Munoz <amunozhe@redhat.com>
2024-02-27 13:16:55 +0100

    format

--- src/main/java/org/rock/spring/SpringGuitarHeroApplication.java
+++ src/main/java/org/rock/spring/SpringGuitarHeroApplication.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.web.client.RestTemplate;
 
-
 @SpringBootApplication
 public class SpringGuitarHeroApplication {
 
